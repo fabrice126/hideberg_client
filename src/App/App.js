fe98049dfc3780193309afc8401d0b536497c37c
fabrice126 <fjauvat@gmail.com>
2018-04-15 13:41:15 +0200

    analytic fonctionne

--- src/App/App.js
+++ src/App/App.js
@@ -10,27 +10,30 @@ import Page404 from '../views/Page404/Page404';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HBheader from '../components/HBheader/HBheader';
 import HBfooter from '../components/HBfooter/HBfooter';
+import withTracker from './HBTracker';
+
 
 export default class App extends Component {
+
   render() {
     return (
       <BrowserRouter>
         <main>
           <div className="content">
-          <div className="divHeader">
-            <HBheader />
-          </div>
+            <div className="divHeader">
+              <HBheader />
+            </div>
             <Switch>
-              <Route exact path='/' component={Continent} />
-              <Route exact path='/continent/:continent' component={Continent} />
-              <Route exact path='/country/:country/sector/:sector' component={Home} />
+              <Route exact path='/' component={withTracker(Continent)} />
+              <Route exact path='/continent/:continent' component={withTracker(Continent)} />
+              <Route exact path='/country/:country/sector/:sector' component={withTracker(Home)} />
 
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/*' component={Page404} />
+              <Route exact path='/login' component={withTracker(Login)} />
+              <Route exact path='/*' component={withTracker(Page404)} />
             </Switch>
           </div>
           <div className="divFooter">
-            <HBfooter  />
+            <HBfooter />
           </div>
         </main>
       </BrowserRouter>
